86137ab5e00ac085e6578a1f3dbf293323a36c78
Anas Saidi <anassaidi1337@gmail.com>
2022-12-04 22:55:02 +0100

    no more empty methods

--- src/main/java/ensa/project_vt/SearchViewController.java
+++ src/main/java/ensa/project_vt/SearchViewController.java
@@ -16,6 +16,16 @@ public class SearchViewController {
    //method to be called when search button is clicked
    public void search(ActionEvent a){
        searchInput = searchField.getText();
+       // check if the input is empty and return
+
+       // get the type of input : is it a link or a keyword ?
+       String type = getInputType(searchInput);
+       switch (type) {
+           case "yt-link" -> System.out.println("It's a youtube link");
+           case "link" -> System.out.println("It's not a youtube link");
+           case "keyword" -> System.out.println("Searching for keyword now");
+           default -> System.out.println("Invalid input");
+       }
 
    }
    public void createResultView(String title,String duration){
@@ -25,7 +35,14 @@ public class SearchViewController {
             // check if input is a valid url
             if(isValidURL(input)){
                 // check if it's a yt url
-
+                if(isYtUrl(input))
+                {
+                    return "yt-link";
+                }
+                else return "link";
+            }
+            else {
+                return "keyword";
             }
    }
    public boolean isValidURL(String url){
@@ -36,11 +53,6 @@ public class SearchViewController {
                + "._\\+~#?&//=]*)";
        // Compile the ReGex
        Pattern p = Pattern.compile(regex);
-       // If the string is empty
-       // return false
-       if (url == null) {
-           return false;
-       }
        // Find match between given string
        // and regular expression
        // using Pattern.matcher()
@@ -48,8 +60,12 @@ public class SearchViewController {
        return m.matches();
    }
    // return true if the argument is url
-   public boolean isYtUrl(){
-
+   public boolean isYtUrl(String url){
+       String regex = "^((?:https?:)?\\/\\/)?((?:www|m)\\.)?((?:youtube\\.com|youtu.be))(\\/(?:[\\w\\-]+\\?v=|embed\\/|v\\/)?)([\\w\\-]+)(\\S+)?$";
+       // Compile the ReGex
+       Pattern p = Pattern.compile(regex);
+       Matcher m = p.matcher(url);
+       return m.matches();
    }
    public String getSearchInput(){
        return this.searchInput;
